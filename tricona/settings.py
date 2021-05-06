52f3381422df0abcb541a0fca49dc4b63d8c7eae
Your Name <saurabh.mishra@collegedunia.com>
2021-05-06 17:36:55 +0530

    asdvf

--- tricona/settings.py
+++ tricona/settings.py
@@ -118,9 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",]
+    BASE_DIR / "static",
+]
+STATIC_ROOT = "../static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
